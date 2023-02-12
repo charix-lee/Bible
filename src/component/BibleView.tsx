2d2c1b0b2aa22d6732c30c61d3c1b2babfd41e90
Jaehee Rhee <jhrhee@companylabs.co.kr>
2023-02-12 19:58:06 +0900

    Test header working
    - 헤더 비슷한 기능 추가

--- src/component/BibleView.tsx
+++ src/component/BibleView.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Chang01 } from './Constant';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export const BibleView = () => {
+  const screenHeight = Dimensions.get("window").height - 170;
+  const backgroundStyle = {
+    backgroundColor: Colors.white,
+    paddingTop: 26,
+    maxHeight: screenHeight,
+  };
+
   return (
-    <View style={styles.frame}>
-      {Chang01.map((item, index) =>
-        <Text style={styles.text}>{item}</Text>
-      )}
-    </View>
+    <ScrollView
+      contentInsetAdjustmentBehavior={'automatic'}
+      style={backgroundStyle}
+    >
+      <View style={styles.body}>
+        {Chang01.map((item, index) =>
+          <Text style={styles.text}>{item}</Text>
+        )}
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  body: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
   frame: {
     width: '100%',
     height: '100%',
