6cef51c122380cb107338f04e60c42295919ad62
Jaehee Rhee <jhrhee@companylabs.co.kr>
2023-04-12 00:06:38 +0900

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['import'],
   rules: {
     'object-curly-spacing': ['error', 'always'],
-    'brace-style': 0,
-    'id-blacklist': 0,
-    curly: 0,
+  },
+  env: {
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
   },
   // plugins: ['react-svg'],
-  // "parserOptions": {
-  //   "ecmaVersion": 2017,
-  // },
-  // "env": {
-  //   "es6": true,
-  // },
 };
