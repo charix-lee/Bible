d301b234df3d1e412b2e7f09bb51585ca11cc76b
Jaehee Rhee <jhrhee@companylabs.co.kr>
2023-04-12 00:07:38 +0900

    Fix eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+  },
+  env: {
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
 };
