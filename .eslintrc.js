552b84116b81c76ef7c01ca872a83555b7e62e5f
Jaehee Rhee <jhrhee@companylabs.co.kr>
2023-04-11 23:48:22 +0900

    Fix settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    'brace-style': 0,
+    'id-blacklist': 0,
+    curly: 0,
+  },
+  // plugins: ['react-svg'],
+  // "parserOptions": {
+  //   "ecmaVersion": 2017,
+  // },
+  // "env": {
+  //   "es6": true,
+  // },
 };
